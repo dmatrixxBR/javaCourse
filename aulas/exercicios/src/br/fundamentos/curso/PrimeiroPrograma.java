603077e7f32b98b9d6c056e09eb3273583e7f5b8
dmatrixx <dmatrixx@gmail.com>
2022-03-04 17:52:59 -0300

    aula 01 estrutura

---
+++ aulas/exercicios/src/br/fundamentos/curso/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.fundamentos.curso;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+	    System.out.println("Welcome Java Developers");
+	    System.out.println("Welcome Java Developers - 01");
+	    System.out.println("Welcome Java Developers - 02");
+	    
+	    
+	}
+}
